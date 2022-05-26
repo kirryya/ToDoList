a6ced94db45196a674b2eef1f7ec07c7a75d2a4c
kirryya <kir1987@inbox.ru>
2022-05-26 22:30:44 +0300

    react-router-dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,6 +8,8 @@ import LinearProgress from "@mui/material/LinearProgress";
 import {useSelector} from "react-redux";
 import {AppRootStateType} from "./store";
 import {RequestStatusType} from "./app-reducer";
+import {Navigate, Route, Routes} from 'react-router-dom';
+import {Login} from "../features/Login";
 
 export function App() {
 
@@ -19,7 +21,12 @@ export function App() {
             <ButtonAppBar/>
             {status === "loading" && <LinearProgress/>}
             <Container fixed>
-                <TodolistsList/>
+                <Routes>
+                    <Route path={"/"} element={<TodolistsList/>}/>
+                    <Route path={"/login"} element={<Login/>}/>
+                    <Route path={"/404"} element={<h1 style={{textAlign: "center"}}>404: Page Not Found</h1>}/>
+                    <Route path={"*"} element={<Navigate to={"/404"}/>}/>
+                </Routes>
             </Container>
         </div>
     )
