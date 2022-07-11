c0b3f67dbbbda28f1a8246aa33b0a3cb2427ef68
kirryya <kir1987@inbox.ru>
2022-07-11 22:24:39 +0300

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,13 @@ import './index.css';
 import {App} from "./app/App";
 import {Provider} from "react-redux";
 import {store} from "./app/store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')!)
     .render(
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <App/>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     )
